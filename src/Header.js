8932fedc0bdfeac0cbca3273f19fa66c59c16c5d
angor78 <angor78@gmail.com>
2021-08-08 18:38:26 +0300

    Components to directory

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div>
+      <a href="#s">Home</a><br/>
+      <a href="#s">Feed</a>
+      <a href="#s">Help</a>
+    </div>
+  );
+};
+export default Header
---
+++ src/Technologies.js
@@ -0,0 +1,13 @@
+const Technologies = () => {
+    return (
+      <div className="App">
+        <ul>
+          <li>dasd</li>
+          <li>dasd</li>
+          <li>dasd</li>
+          <li>dasd</li>
+        </ul>
+      </div>
+    );
+  };
+  export default Technologies
