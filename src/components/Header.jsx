a597c90f84b535c637523fa6804ca787893b987c
angor78 <angor78@gmail.com>
2021-08-16 00:12:18 +0300

    lesson13

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return <header className="header">
+    <img
+      alt="logo"
+      src="https://img3.goodfon.ru/wallpaper/nbig/7/61/spanch-bob-gubka-glaza-ochki.jpg"
+    />
+  </header>;
+};
+  
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Profile = () => {
+  return <div className="content">
+  <div>
+    <img
+      alt="content"
+      src="https://pbs.twimg.com/profile_banners/2882905954/1416337076/1500x500"
+    />
+  </div>
+  <div>
+    ava + description
+    <img
+      width="300"
+      alt="avatar"
+      src="https://yt3.ggpht.com/a/AGF-l7-hPMNZV0jJsCFwwC4W6QUiN8bwykm_jTFX5w=s900-c-k-c0xffffffff-no-rj-mo"
+    />
+  </div>
+  <div>
+    My posts
+    <div>New post</div>
+    <div>1 post</div>
+    <div>2 post</div>
+  </div>
+</div>
+
+};
+  
+export default Profile;
