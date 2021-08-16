2a7969cf734d3b7681acf137305fcf6d68373207
angor78 <angor78@gmail.com>
2021-08-16 15:26:55 +0300

    Lesson14

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import s from './Header.module.css'
 
 const Header = () => {
-  return <header className="header">
+  return <header className={s.header}>
     <img
       alt="logo"
       src="https://img3.goodfon.ru/wallpaper/nbig/7/61/spanch-bob-gubka-glaza-ochki.jpg"
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,22 @@
 import React from "react";
-
+import s from './Navbar.module.css'
+console.log('s', s);
 const Navbar = () => {
-  return <nav className="nav">
-  <div>
+  return <nav className={s.nav}>
+  <div className={`${s.item} ${s.active}`}>
     <a href="#s">Profile</a>
   </div>
-  <div>
-    <a href="#s">Mesagges</a>
+  <div className={`${s.item}`}>
+    <a href="#s">Messages</a>
   </div>
-  <div>
+  <div className={`${s.item}`}>
     <a href="#s">News</a>
   </div>
-  <div>
+  <div className={`${s.item}`}>
     <a href="#s">Music</a>
   </div>
-  <div>
-    <a href="#s">Setings</a>
+  <div className={`${s.item}`}>
+    <a href="#s">Settings</a>
   </div>
 </nav>
 };
