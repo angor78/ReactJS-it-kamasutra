abdb97271bba5e119f597a563d09264d06869562
angor78 <angor78@gmail.com>
2021-08-16 19:45:12 +0300

    Lesson16

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
-const MyPosts = () => {
+const MyPosts = (props) => {
+  //debugger;
   return (
     <div>
       My posts
@@ -11,11 +12,8 @@ const MyPosts = () => {
         <button>Add post</button>
       </div>
       <div className={s.posts}>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        <Post message="Hi, how are you?" countLike ='11'/>
+        <Post message="It's my first post!" countLike='2'/>
       </div>
     </div>
   );
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import s from "./Post.module.css";
 
-const Post = () => {
+const Post = (props) => {
+  debugger;
   return (
     <div className={s.item}>
       <img
         alt="ava"
         src="https://im0-tub-ru.yandex.net/i?id=2d001fc315acc21401601db5f9e46d4d&n=13&exp=1"
       />
-      1 post
+      {props.message}
       <div>
-        <span>Like</span>
+        <span>Likes {props.countLike}</span>
       </div>
     </div>
   );
