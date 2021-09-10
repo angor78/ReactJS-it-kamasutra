cfd12c0608794bfb77ec26537e02498eddfd7ff7
angor78 <angor78@gmail.com>
2021-09-10 10:58:15 +0300

    Lesson55_Pagination

--- src/redux/users-reduser.js
+++ src/redux/users-reduser.js
@@ -1,9 +1,14 @@
 const FOLLOW = "FOLLOW";
 const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
+const SET_CURRENT_PAGE = "SET_CURRENT_PAGE";
+const SET_TOTAL_USERS_COUNT = "SET_TOTAL_USERS_COUNT";
 
 const initialState = {
   users: [],
+  totalUsersCount: 21,
+  pageSize: 5,
+  currentPage: 4,
 };
 
 const usersReduser = (state = initialState, action) => {
@@ -29,7 +34,13 @@ const usersReduser = (state = initialState, action) => {
         }),
       };
     case SET_USERS:
-      return { ...state, users: [...state.users, ...action.users] };
+      return { ...state, users: action.users };
+
+    case SET_CURRENT_PAGE:
+      return { ...state, currentPage: action.currentPage };
+
+    case SET_TOTAL_USERS_COUNT:
+      return { ...state, totalUsersCount: action.count };
 
     default:
       return state;
@@ -46,6 +57,14 @@ export const unFollowUserAC = (userId) => ({
 export const setUsers = (users) => ({
   type: SET_USERS,
   users,
+});
+export const setCurrentPageAC = (currentPage) => ({
+  type: SET_CURRENT_PAGE,
+  currentPage,
+});
+export const setUsersTotalCountAC = (totalUsersCount) => ({
+  type: SET_TOTAL_USERS_COUNT,
+  count: totalUsersCount,
 });
 
 export default usersReduser;
